431c8348deb4f5150b9ca69f5a55b605d5c1a69c
Luca Corti <luca@sibill.com>
2022-06-06 11:55:01 +0200

    Remove redundant implementations for Resource.Loadable

--- lib/jsonapi/resource/loadable.ex
+++ lib/jsonapi/resource/loadable.ex
@@ -16,14 +16,6 @@ defimpl JSONAPI.Resource.Loadable, for: Atom do
   def loaded?(_atom), do: true
 end
 
-defimpl JSONAPI.Resource.Loadable, for: List do
-  def loaded?(_resource), do: true
-end
-
-defimpl JSONAPI.Resource.Loadable, for: Map do
-  def loaded?(_resource), do: true
-end
-
 defimpl JSONAPI.Resource.Loadable, for: Any do
   def loaded?(_resource), do: true
 end
