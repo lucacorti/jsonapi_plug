b6d6dabff5595acb2dc94578239cc4720fea05ac
Luca Corti <luca@fantacast.it>
2023-08-18 20:51:59 +0200

    Use OTP 26 / Elixir 1.15

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule JSONAPIPlug.Mixfile do
       version: "1.0.3",
       package: package(),
       description: "JSON:API library for Plug and Phoenix applications",
-      elixir: "~> 1.11",
+      elixir: "~> 1.12",
       elixirc_paths: elixirc_paths(Mix.env()),
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
